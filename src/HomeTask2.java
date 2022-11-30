3fa6c04aad8be20e0dddcd1b7a6b5ab456bec22a
Valery <valery.ansimova@gmail.com>
2022-11-30 17:21:55 +0100

    task1.2

---
+++ src/HomeTask2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HomeTask2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How are you?");
+        String answer = sc.next();
+        System.out.println("You are " +answer);
+
+    }
+}
